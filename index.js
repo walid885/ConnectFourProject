049c630b593e845c299c1e776d9eae429b4d503a
walid885 <bettaieb88walid@gmail.com>
2023-03-10 15:11:36 +0100

    the functional code is working

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ document.addEventListener('DOMContentLoaded',()=>{
     for ( let i =0 ; i < squares.length ; i++){
         squares[i].onclick =() => {
            // alert("you have clicked square " + i)
-           // if the square below your current square is taken you can go on top of it 
+           // if the square below your current square is taken you can go on top of it             
            if ( squares[i+7].classList.contains('taken')){
-            if(currentPlayer ==1 ){
+            if(currentPlayer == 1 ){
                 squares[i].classList.add('taken')
                 squares[i].classList.add('player-one')
                 currentPlayer = 2 
                 displayCurrentPlayer.innerHTML = currentPlayer
             }
-           }
+                else if (currentPlayer == 2 ){
+                squares[i].classList.add('taken')
+                squares[i].classList.add('player-two')
+                currentPlayer = 1 
+                displayCurrentPlayer.innerHTML = currentPlayer
+            }
+           } else {alert('You cant go here ! ')}
         }
     }
 
